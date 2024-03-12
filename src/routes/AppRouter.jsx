269dd851d08afe69b9b76055ece54c188adab28d
Edgar Joaquin Flores Gonzalez <floresedgarjoaquin@gmail.com>
2024-03-12 13:11:53 -0600

    Vid 408 - Creación de variables de entorno

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate }  from 'react-router-dom'
 import { LoginPage } from '../auth/pages/LoginPage'
 import { CalendarPage } from '../calendar/pages/CalendarPage'
+import { getEnvVariables } from '../helpers'
 
 
 export const AppRouter = () => {
