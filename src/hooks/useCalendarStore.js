e94887cc30f6ebb8a1e4c08ca9af31a7d29b9eb6
Edgar Joaquin Flores Gonzalez <floresedgarjoaquin@gmail.com>
2024-03-19 13:54:53 -0600

    Vid 423 - Creacion de nuevo evento de calendario en backen y front

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,11 +1,13 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from '../store/calendar/calendarSlice';
+import { calendarApi } from '../api';
 
 
 export const useCalendarStore = ()=> {
 
     const dispatch = useDispatch()
     const { events, activeEvent } = useSelector( state => state.calendar );
+    const { user } = useSelector( state => state.auth );
 
     const setActiveEvent = ( calendarEvent )=> {
         dispatch( onSetActiveEvent(calendarEvent) )
@@ -13,13 +15,18 @@ export const useCalendarStore = ()=> {
 
     const startSavingEvent = async( calendarEvent )=> {
         //TODO:Ir a backend
+
+
         //*si pasa
         if( calendarEvent._id ){
             //*actualizar evento si existe un_id
             dispatch( onUpdateEvent({ ...calendarEvent } ) )
         }else{
             //*creando evento
-            dispatch( onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() } ) )
+            const {data} = await calendarApi.post('/event', calendarEvent)
+            console.log(data);
+
+            dispatch( onAddNewEvent({ ...calendarEvent, id: data.eventCreated.id, user } ) )
         }
     }
 
