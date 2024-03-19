0ace5b48aed54f30dc2f91e7b3289072027555ec
Edgar Joaquin Flores Gonzalez <floresedgarjoaquin@gmail.com>
2024-03-18 19:08:06 -0600

    Vid 413/414 - Despachar acciones y mostrar error en la autenticación

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { calendarApi } from '../api';
+import { onChecking, onLogin, onLogout } from '../store/auth/authSlice';
 
 
 export const useAuthStore = () => {
@@ -10,9 +11,19 @@ const dispatch = useDispatch();
 const startLogin = async({ email, password })=> {
     try {
         const res = await calendarApi.post('/auth/login', { email, password });
-        console.log({res});
+        dispatch(onChecking());
+        localStorage.setItem('token', res.data.token);
+        localStorage.setItem('token-init-date', new Date().getTime());
+        dispatch(onLogin( {name: res.data.name, uid: res.data.uid} ));
+
+        return res.data.user;
     } catch (error) {
         console.log(error);
+        dispatch(onLogout('Credenciales incorrectas'));
+        setTimeout(() => {
+            dispatch(onLogout(undefined));
+        }
+        , 10);
     }
 }
 
