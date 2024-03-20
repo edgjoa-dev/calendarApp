960138ff4066e97bca17112142793fa568fdf3d3
Edgar Joaquin Flores Gonzalez <floresedgarjoaquin@gmail.com>
2024-03-19 21:05:47 -0600

    Deploy - HashRouter

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Provider }  from 'react-redux'
 
 import { AppRouter } from "./routes"
